6b930ffaf70f0303748141fa2dee675a05209549
wujiGeng <wuji.geng@gmail.com>
2017-04-19 22:55:55 +0200

    Add files via upload

---
+++ app/server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/partials', express.static(__dirname + '/partials'));
+
+
+app.all('/*', function(req, res, next) {
+    // Just send the index.html for other files to support HTML5Mode
+    res.sendFile('index.html', { root: __dirname });
+});
+
+app.listen(process.env.PORT || 8000); //the port you want to use
